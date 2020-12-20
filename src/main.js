2783b6d1e5a7002916e09dd41550e95adee8f839
Rustam Gainulin <17axaH@mail.ru>
2020-12-20 19:02:38 +0100

    npm

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
 import Vue from 'vue';
-import router from '@/router';
 import App from './App.vue';
-
-import VueModal from './lib/index';
-
-Vue.use(VueModal);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
   render: (h) => h(App),
 }).$mount('#app');
