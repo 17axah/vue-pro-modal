c8e1871b09c57380c9e5e2c96c79f04d9fefcc55
Rustam Gainulin <17axaH@mail.ru>
2020-12-26 01:28:14 +0100

    corrections

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import router from '@/router';
 import App from './App.vue';
 
+import VueModal from './lib/index';
+
+Vue.use(VueModal);
+
 Vue.config.productionTip = false;
 
 new Vue({
